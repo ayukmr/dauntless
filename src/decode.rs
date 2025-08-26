9d92f12c95cb37b8bc29f2b82d1b6235f11da919
Ayush Kumar <github@ayukmr.com>
2025-08-26 14:14:15 -0700

    Use partitioning for decoding

--- src/decode.rs
+++ src/decode.rs
@@ -1,6 +1,6 @@
-use crate::types::{Lightness, Corners};
+use crate::types::{Lightness, Corners, FPoint, FCorners};
 
-use ndarray::{s, Array1, Array2, Zip};
+use ndarray::{s, Array2};
 
 const CODES: [u64; 11] = [
     57401312644,
@@ -16,13 +16,13 @@ const CODES: [u64; 11] = [
     14225578886,
 ];
 
-const TAG_SIZE: usize = 6;
+pub fn decode(img: &Lightness, corners: Corners) -> Option<u32> {
+    let tag = sample(img, corners);
 
-pub fn decode(img: &Lightness, points: Corners) -> Option<u8> {
-    let tag = sample(img, points);
+    let min = tag.fold(f32::INFINITY, |a, &b| a.min(b));
+    let max = tag.fold(f32::NEG_INFINITY, |a, &b| a.max(b));
 
-    let max = tag.iter().cloned().reduce(f32::max).unwrap();
-    let mut bits = (&tag / max).mapv(|l| l > 0.5);
+    let mut bits = (&(tag - min) / (max - min)).mapv(|l| l > 0.75);
 
     for _ in 0..4 {
         let bin =
@@ -33,7 +33,7 @@ pub fn decode(img: &Lightness, points: Corners) -> Option<u8> {
         let id = CODES.iter().position(|&i| i == bin);
 
         if let Some(id) = id {
-            return Some(id as u8);
+            return Some(id as u32);
         }
 
         bits = rot90(bits);
@@ -42,39 +42,103 @@ pub fn decode(img: &Lightness, points: Corners) -> Option<u8> {
     None
 }
 
-fn sample(img: &Lightness, corners: Corners) -> Lightness {
-    let idx = Array1::from_shape_fn(
-        TAG_SIZE,
-        |i| (i as f32 + 1.5) / (TAG_SIZE + 2) as f32,
+fn rot90<T: Clone>(a: Array2<T>) -> Array2<T> {
+    a.slice(s![..;-1, ..]).reversed_axes().to_owned()
+}
+
+fn sample(img: &Lightness, corners: Corners) -> Array2<f32> {
+    let (tl, tr, bl, br) = corners;
+
+    let points = partition(
+        (
+            (tl.0 as f32, tl.1 as f32),
+            (tr.0 as f32, tr.1 as f32),
+            (bl.0 as f32, bl.1 as f32),
+            (br.0 as f32, br.1 as f32),
+        ),
+        3,
     );
 
-    let u = Array2::from_shape_fn((TAG_SIZE, TAG_SIZE), |(_, x)| idx[x]);
-    let v = Array2::from_shape_fn((TAG_SIZE, TAG_SIZE), |(y, _)| idx[y]);
+    let bits: Vec<f32> = points.iter().map(|pt| {
+        img[(pt.1 as usize, pt.0 as usize)]
+    }).collect();
 
-    let inv_u = u.mapv(|x| 1.0 - x);
-    let inv_v = v.mapv(|x| 1.0 - x);
+    let dirs = [(0, 0), (1, 0), (0, 1), (1, 1)];
+
+    let mut out = vec![0.0; 64];
+    let mut idx = 0;
+
+    for (bx, by) in &dirs {
+        for (cx, cy) in &dirs {
+            for (x, y) in &dirs {
+                let pos = x + cx * 2 + bx * 4 + (y + cy * 2 + by * 4) * 8;
+                out[pos] = bits[idx];
+
+                idx += 1;
+            }
+        }
+    }
+
+    Array2::from_shape_vec((8, 8), out).unwrap().slice_move(s![1..7, 1..7])
+}
+
+fn partition(corners: FCorners, depth: u32) -> Vec<FPoint> {
+    let c = center(corners);
+
+    if depth == 0 {
+        return vec![c];
+    }
 
     let (tl, tr, bl, br) = corners;
 
-    let ix = (
-        &inv_u * &inv_v * tl.0 as f32 +
-        &u * &inv_v * tr.0 as f32 +
-        &u * &v * br.0 as f32 +
-        &inv_u * &v * bl.0 as f32
-    ).mapv(|x| x as usize);
+    let left = (tl.0 + bl.0) / 2.0;
+    let right = (tr.0 + br.0) / 2.0;
 
-    let iy = (
-        &inv_u * &inv_v * tl.1 as f32 +
-        &u * &inv_v * tr.1 as f32 +
-        &u * &v * br.1 as f32 +
-        &inv_u * &v * bl.1 as f32
-    ).mapv(|x| x as usize);
+    let width = right - left;
 
-    Zip::from(&ix)
-        .and(&iy)
-        .map_collect(|&x, &y| img[[y, x]])
+    let left_h = bl.1 - tl.1;
+    let right_h = br.1 - tr.1;
+
+    let center_h = mix(
+        (c.0 - left) / width,
+        left_h,
+        right_h,
+    );
+
+    let tm = (c.0, c.1 - center_h / 2.0);
+    let bm = (c.0, c.1 + center_h / 2.0);
+
+    let m1 = (tr.1 - tl.1) / (tr.0 - tl.0);
+    let m2 = (br.1 - bl.1) / (br.0 - bl.0);
+
+    let avg = (m1 + m2) / 2.0;
+
+    let ml = (left,  c.1 - avg * (c.0 - left));
+    let mr = (right, c.1 + avg * (right - c.0));
+
+    vec![
+        partition((tl, tm, ml, c), depth - 1),
+        partition((tm, tr, c, mr), depth - 1),
+        partition((ml, c, bl, bm), depth - 1),
+        partition((c, mr, bm, br), depth - 1),
+    ].into_iter().flatten().collect()
 }
 
-fn rot90<T: Clone>(a: Array2<T>) -> Array2<T> {
-    a.slice(s![..;-1, ..]).reversed_axes().to_owned()
+fn center(corners: FCorners) -> FPoint {
+    let (tl, tr, bl, br) = corners;
+
+    let m1 = (br.1 - tl.1) / (br.0 - tl.0);
+    let m2 = (bl.1 - tr.1) / (bl.0 - tr.0);
+
+    intersect(m1, tl, m2, tr)
+}
+
+fn intersect(m1: f32, p1: FPoint, m2: f32, p2: FPoint) -> FPoint {
+    let x = (m2 * p2.0 - m1 * p1.0 + p1.1 - p2.1) / (m2 - m1);
+
+    (x, m1 * (x - p1.0) + p1.1)
+}
+
+fn mix(x: f32, a: f32, b: f32) -> f32 {
+    a + x * (b - a)
 }
--- src/types.rs
+++ src/types.rs
@@ -6,6 +6,10 @@ pub type Frequency = Array2<Complex<f32>>;
 pub type Mask = Array2<bool>;
 
 pub type Point = (u32, u32);
+pub type Corners = (Point, Point, Point, Point);
+
 pub type Points = Vec<Point>;
 pub type Shapes = Vec<Points>;
-pub type Corners = (Point, Point, Point, Point);
+
+pub type FPoint = (f32, f32);
+pub type FCorners = (FPoint, FPoint, FPoint, FPoint);
