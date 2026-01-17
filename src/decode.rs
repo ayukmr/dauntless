763c1a6b0e24b66be5556b4e468166dfb29e058c
Ayush Kumar <github@ayukmr.com>
2026-01-17 10:37:25 -0800

    Use homography decode

--- src/decode.rs
+++ src/decode.rs
@@ -1,4 +1,4 @@
-use crate::types::{Lightness, Corners, FPoint, FCorners};
+use crate::types::{Lightness, Corners, FCorners};
 
 use ndarray::{s, Array2};
 
@@ -49,99 +49,70 @@ fn rot90<T: Clone>(a: Array2<T>) -> Array2<T> {
 }
 
 fn sample(img: &Lightness, corners: Corners) -> Option<Array2<f32>> {
-    let (tl, tr, bl, br) = corners;
+    let hm = Homography::from_corners((
+        (corners.0.0 as f32, corners.0.1 as f32),
+        (corners.1.0 as f32, corners.1.1 as f32),
+        (corners.2.0 as f32, corners.2.1 as f32),
+        (corners.3.0 as f32, corners.3.1 as f32),
+    ));
 
-    let points = partition(
-        (
-            (tl.0 as f32, tl.1 as f32),
-            (tr.0 as f32, tr.1 as f32),
-            (bl.0 as f32, bl.1 as f32),
-            (br.0 as f32, br.1 as f32),
-        ),
-        3,
-    );
+    let mut out = vec![0.0; 36];
 
-    let vals: Vec<&f32> = points.iter().map(|pt| {
-        img.get((pt.1 as usize, pt.0 as usize))
-    }).collect::<Option<Vec<&f32>>>()?;
+    for y in 0..6 {
+        for x in 0..6 {
+            let u = (x as f32 + 1.5) / 8.0;
+            let v = (y as f32 + 1.5) / 8.0;
 
-    let dirs = [(0, 0), (1, 0), (0, 1), (1, 1)];
+            let (ix, iy) = hm.map(u, v);
 
-    let mut out = vec![0.0; 64];
-    let mut idx = 0;
+            let px = ix.floor() as isize;
+            let py = iy.floor() as isize;
 
-    for (bx, by) in &dirs {
-        for (cx, cy) in &dirs {
-            for (x, y) in &dirs {
-                let pos = x + cx * 2 + bx * 4 + (y + cy * 2 + by * 4) * 8;
-                out[pos] = *vals[idx];
-
-                idx += 1;
-            }
+            let val = img.get((py as usize, px as usize))?;
+            out[x + y * 6] = *val;
         }
     }
 
-    let ary = Array2::from_shape_vec((8, 8), out).unwrap().slice_move(s![1..7, 1..7]);
-    Some(ary)
+    Some(Array2::from_shape_vec((6, 6), out).unwrap())
 }
 
-fn partition(corners: FCorners, depth: u32) -> Vec<FPoint> {
-    let c = center(corners);
+struct Homography {
+    mat: [f32; 9],
+}
 
-    if depth == 0 {
-        return vec![c];
+impl Homography {
+    fn from_corners(corners: FCorners) -> Homography {
+        let ((x0, y0), (x1, y1), (x2, y2), (x3, y3)) = corners;
+
+        let dx1 = x1 - x3;
+        let dx2 = x2 - x3;
+        let dx3 = x0 - x1 + x3 - x2;
+
+        let dy1 = y1 - y3;
+        let dy2 = y2 - y3;
+        let dy3 = y0 - y1 + y3 - y2;
+
+        let denom = dx1 * dy2 - dx2 * dy1;
+
+        let g = (dx3 * dy2 - dx2 * dy3) / denom;
+        let h = (dx1 * dy3 - dx3 * dy1) / denom;
+
+        Homography {
+            mat: [
+                x1 - x0 + g * x1, x2 - x0 + h * x2, x0,
+                y1 - y0 + g * y1, y2 - y0 + h * y2, y0,
+                g, h, 1.0,
+            ],
+        }
     }
 
-    let (tl, tr, bl, br) = corners;
+    fn map(&self, u: f32, v: f32) -> (f32, f32) {
+        let m = &self.mat;
 
-    let left = (tl.0 + bl.0) / 2.0;
-    let right = (tr.0 + br.0) / 2.0;
+        let x = m[0] * u + m[1] * v + m[2];
+        let y = m[3] * u + m[4] * v + m[5];
+        let w = m[6] * u + m[7] * v + m[8];
 
-    let width = right - left;
-
-    let left_h = bl.1 - tl.1;
-    let right_h = br.1 - tr.1;
-
-    let center_h = mix(
-        (c.0 - left) / width,
-        left_h,
-        right_h,
-    );
-
-    let tm = (c.0, c.1 - center_h / 2.0);
-    let bm = (c.0, c.1 + center_h / 2.0);
-
-    let m1 = (tr.1 - tl.1) / (tr.0 - tl.0);
-    let m2 = (br.1 - bl.1) / (br.0 - bl.0);
-
-    let avg = (m1 + m2) / 2.0;
-
-    let ml = (left,  c.1 - avg * (c.0 - left));
-    let mr = (right, c.1 + avg * (right - c.0));
-
-    vec![
-        partition((tl, tm, ml, c), depth - 1),
-        partition((tm, tr, c, mr), depth - 1),
-        partition((ml, c, bl, bm), depth - 1),
-        partition((c, mr, bm, br), depth - 1),
-    ].into_iter().flatten().collect()
-}
-
-fn center(corners: FCorners) -> FPoint {
-    let (tl, tr, bl, br) = corners;
-
-    let m1 = (br.1 - tl.1) / (br.0 - tl.0);
-    let m2 = (bl.1 - tr.1) / (bl.0 - tr.0);
-
-    intersect(m1, tl, m2, tr)
-}
-
-fn intersect(m1: f32, p1: FPoint, m2: f32, p2: FPoint) -> FPoint {
-    let x = (m2 * p2.0 - m1 * p1.0 + p1.1 - p2.1) / (m2 - m1);
-
-    (x, m1 * (x - p1.0) + p1.1)
-}
-
-fn mix(x: f32, a: f32, b: f32) -> f32 {
-    a + x * (b - a)
+        (x / w, y / w)
+    }
 }
