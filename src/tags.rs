4c6cc1527e5f05e41da3f722245d5c437b7b73a9
Ayush Kumar <github@ayukmr.com>
2026-01-17 13:38:33 -0800

    Formatting

--- src/tags.rs
+++ src/tags.rs
@@ -6,10 +6,12 @@ pub fn tags(edges: &Mask, corners: &Mask) -> Quads {
     let shapes = shapes::find_shapes(edges, corners);
     let mut res = shapes::filter_quads(shapes);
 
-    if cfg().filter_paras {
+    let cfg = cfg();
+
+    if cfg.filter_paras {
         res = filters::filter_paras(res);
     }
-    if cfg().filter_enclosed {
+    if cfg.filter_enclosed {
         res = filters::filter_enclosed(res);
     }
 
